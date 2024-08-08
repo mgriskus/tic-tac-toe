d8edc15377f5648c7df6f23fa2a8ffd94d830103
Matas Griškus <135244071+mgriskus@users.noreply.github.com>
2024-08-08 19:16:27 +0300

    Add user interface

--- script.js
+++ script.js
@@ -1,112 +1,149 @@
-const ticTacToe = function() {
+const ticTacToe = function () {
     function makePlayer(marker) {
         let playerMarker = marker;
         function getMarker() {
             return playerMarker;
         }
-        return {getMarker};
+        return { getMarker };
     }
 
-    const gameboard = function() {
+    const player1 = makePlayer('X');
+    const player2 = makePlayer('Y');
+
+    const gameboard = function () {
         let board;
-        function resetBoard() {
-            board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']];
+        let active = false;
+        function reset() {
+            board = [['', '', ''], ['', '', ''], ['', '', '']];
+            active = true;
         }
-        resetBoard();
-        function showBoard() {
-            return board;
+        reset();
+        function deactivate() {
+            active = false;
         }
-        function markTile(row, column, player) {
-            if (board[row][column] != '_') {
+        function markSquare(row, column, player) {
+            if (board[row][column] != '' || !active) {
                 return false;
             }
             board[row][column] = player.getMarker();
             return true;
         }
-        function getTileMarker(row, column) {
+        function getSquareMarker(row, column) {
             return board[row][column];
         }
-        return {resetBoard, showBoard, getTileMarker, markTile};
+        return { reset, getSquareMarker, markSquare, deactivate };
     }();
 
-    const endChecker = function(gameboard) {
+    const turnHandler = function () {
+        let activePlayer = player1;
+        let turnsLeft = 9;
+        function getActivePlayer() {
+            return activePlayer;
+        }
+        function switchActivePlayer() {
+            activePlayer = (activePlayer == player1 ? player2 : player1);
+            userInterface.updateInfo();
+            --turnsLeft;
+        }
+        function getTurnsLeft() {
+            return turnsLeft;
+        }
+        function reset() {
+            activePlayer = player1;
+            turnsLeft = 9;
+        }
+        return { getActivePlayer, switchActivePlayer, getTurnsLeft, reset };
+    }();
+
+    const endChecker = function () {
         function checkRow(row) {
             for (let column = 1; column < 3; ++column) {
-                if (gameboard.getTileMarker(row, 0) != gameboard.getTileMarker(row, column)) return '_';
+                if (gameboard.getSquareMarker(row, 0) != gameboard.getSquareMarker(row, column)) return '';
             }
-            return gameboard.getTileMarker(row, 0);
+            return gameboard.getSquareMarker(row, 0);
         }
         function checkColumn(column) {
             for (let row = 1; row < 3; ++row) {
-                if (gameboard.getTileMarker(0, column) != gameboard.getTileMarker(row, column)) return '_';
+                if (gameboard.getSquareMarker(0, column) != gameboard.getSquareMarker(row, column)) return '';
             }
-            return gameboard.getTileMarker(0, column);
+            return gameboard.getSquareMarker(0, column);
         }
         function checkDiagonal1() {
-            return (gameboard.getTileMarker(0, 0) == gameboard.getTileMarker(1, 1) && gameboard.getTileMarker(1, 1) == gameboard.getTileMarker(2, 2) ? gameboard.getTileMarker(0, 0) : '_');
+            return (gameboard.getSquareMarker(0, 0) == gameboard.getSquareMarker(1, 1) && gameboard.getSquareMarker(1, 1) == gameboard.getSquareMarker(2, 2) ? gameboard.getSquareMarker(0, 0) : '');
         }
         function checkDiagonal2() {
-            return (gameboard.getTileMarker(0, 2) == gameboard.getTileMarker(1, 1) && gameboard.getTileMarker(1, 1) == gameboard.getTileMarker(2, 0) ? gameboard.getTileMarker(0, 2) : '_');
+            return (gameboard.getSquareMarker(0, 2) == gameboard.getSquareMarker(1, 1) && gameboard.getSquareMarker(1, 1) == gameboard.getSquareMarker(2, 0) ? gameboard.getSquareMarker(0, 2) : '');
         }
         function check() {
             for (let i = 0; i < 3; ++i) {
                 let rowResult = checkRow(i);
-                if (rowResult != '_') {
+                if (rowResult != '') {
                     return rowResult;
                 }
                 let columnResult = checkColumn(i)
-                if (columnResult != '_') {
+                if (columnResult != '') {
                     return columnResult;
                 }
             }
             let diagonal1Result = checkDiagonal1();
-            if (diagonal1Result != '_') {
+            if (diagonal1Result != '') {
                 return diagonal1Result;
             }
             let diagonal2Result = checkDiagonal2();
-            if (diagonal2Result != '_') {
+            if (diagonal2Result != '') {
                 return diagonal2Result;
             }
-            return '_';
-        }
-        return {check, checkRow, checkColumn};
-    }(gameboard);
-
-    function playRound(player1, player2) {
-        let turns = 9;
-        let winner = '_';
-        while (winner == '_' && turns > 0) {
-            const currentPlayer = (turns % 2 ? player1 : player2);
-            let row;
-            let column;
-            while (true) {
-                row = prompt(`On which row do you want to place the marker ${currentPlayer.getMarker()}?`);
-                column = prompt(`In which column do you want to place the marker ${currentPlayer.getMarker()}?`);
-                if (gameboard.markTile(row, column, currentPlayer)) break;
-                alert("Tile is already marked. Try again.");
+            if (!turnHandler.getTurnsLeft()) {
+                return "No one";
             }
-            console.table(gameboard.showBoard());
-            --turns;
-            winner = endChecker.check();
+            return '';
         }
-        if (winner == '_') {
-            alert("It's a tie!");
-        } else {
-            alert(`The winner is ${winner}!`);
-        }
-        return winner;
+        return { check, checkRow, checkColumn };
+    }();
+
+    function reset() {
+        gameboard.reset();
+        turnHandler.reset();
+        userInterface.updateInfo();
+        userInterface.showBoard();
+        document.removeEventListener("click", reset);
     }
 
-    function play() {
-        const player1 = makePlayer('X');
-        const player2 = makePlayer('Y');
-        for (let i = 0; i < 3; ++i) {
-            playRound(player1, player2);
-            gameboard.resetBoard();
+    const userInterface = function () {
+        function showBoard() {
+            for (let row = 0; row < 3; ++row) {
+                for (let column = 0; column < 3; ++column) {
+                    document.querySelector(`.row${row}.column${column} div`).textContent = gameboard.getSquareMarker(row, column);
+                }
+            }
         }
-    }
+        function updateInfo() {
+            document.querySelector(".info").textContent = `Turn of ${turnHandler.getActivePlayer().getMarker()}`;
+        }
+        function start() {
+            for (let row = 0; row < 3; ++row) {
+                for (let column = 0; column < 3; ++column) {
+                    const play = () => {
+                        const success = gameboard.markSquare(row, column, turnHandler.getActivePlayer());
+                        if (success) {
+                            turnHandler.switchActivePlayer();
+                            const end = endChecker.check();
+                            if (end != '') {
+                                gameboard.deactivate();
+                                document.querySelector(".info").textContent = `${end} wins! (Double click to play again.)`;
+                                document.addEventListener("dblclick", reset);
+                            }
+                            showBoard();
+                        }
+                    }
+                    document.querySelector(`.row${row}.column${column}`).addEventListener("click", play);
+                }
+            }
+        }
+        return { showBoard, start, updateInfo };
+    }();
 
-    return {play};
+    return { userInterface };
 }();
 
-ticTacToe.play();
+ticTacToe.userInterface.start();
